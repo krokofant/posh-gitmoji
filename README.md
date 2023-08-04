36d2978d243b89d14eff52ed0c6337485e426797
krokofant <krokofant@users.noreply.github.com>
2023-08-04 12:40:14 +0200

    🔥 Remove old AreaPath feature

--- README.md
+++ README.md
@@ -11,37 +11,3 @@ Install-Module posh-gitmoji
 ```
 
 Now you can type `gitmoji <tab>` and `<tab>` to autocomplete any gitmoji
-
-## Additional stuff (Optional)
-
-First install dependencies:
-
-- [Windows Terminal](https://github.com/microsoft/terminal)
-- [Azure CLI](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest) _(optional)_
-
-```powershell
-# Run as admin
-choco install -y microsoft-windows-terminal
-choco install -y azure-cli
-```
-
-To autocomplete Azure DevOps story IDs you need to setup your default azure devops organization and project:
-
-```powershell
-az extension add --name azure-devops
-az login
-az devops configure --defaults organization=$(Read-Host DefaultOrganizationURL) project=$(Read-Host DefaultProjectName)
-```
-
-Then install the module:
-
-```powershell
-Install-Module posh-gitmoji -Scope CurrentUser
-```
-
-By default the module will try and fetch the stories from _a certain AreaPath_ in your default az organization.
-To specify which area path run the following:
-
-```powershell
-gitmoji -AreaPath Example\Area
-```
--- posh-gitmoji/posh-gitmoji.psm1
+++ posh-gitmoji/posh-gitmoji.psm1
@@ -1,38 +1,5 @@
 [pscustomobject[]]$script:EMOJIS = (Get-Content $PSScriptRoot/gitmojis.json | ConvertFrom-Json).gitmojis
 
-$script:Config = @{
-    AreaPath = ""
-}
-
-$ConfigLocation = "$env:USERPROFILE/.config/posh-gitmoji/config.json"
-function SaveConfig { Set-Content $ConfigLocation (ConvertTo-Json $script:Config); return }
-function ReloadConfig { $script:Config = ConvertFrom-Json (Get-Content -Raw $ConfigLocation); return }
-
-if (!(Test-Path $ConfigLocation)) {
-    New-Item -ItemType Directory "$ConfigLocation/.." -ErrorAction Ignore
-    SaveConfig
-}
-ReloadConfig
-
-$script:LastAzureQueryDate = $null
-[System.Management.Automation.Job]$script:AzureQueryJob = $null
-function GetAzureQueryString {
-    @"
-Select [System.Id], [System.Title], [System.State], [System.AreaPath] From WorkItems Where [State] = 'Developing' AND [System.AreaPath] = '$($script:Config.AreaPath)' order by [System.CreatedDate] desc
-"@
-}
-
-
-function GetActiveStories {
-    if (($null -eq $script:LastAzureQueryDate) -or (($(Get-Date) - $script:LastAzureQueryDate).TotalSeconds -gt 60) ) {
-        $script:AzureQueryJob = Start-Job -InputObject $(GetAzureQueryString) -ScriptBlock { [pscustomobject[]]$items = az boards query --wiql $input | ConvertFrom-Json; $items | ForEach-Object { [pscustomobject]@{id = $_.fields.'System.Id'; title = $_.fields.'System.Title'; } } }
-        $script:LastAzureQueryDate = Get-Date
-        $script:AzureQueryJob | Wait-Job | Out-Null
-        return Receive-Job $script:AzureQueryJob -Keep
-    }
-    return Receive-Job $script:AzureQueryJob -Keep
-}
-
 function gitmoji {
     [CmdletBinding()]
     param (
@@ -43,14 +10,8 @@ function gitmoji {
         [Parameter()]
         [switch]$All,
         [Parameter()]
-        [string]$Story = $null,
-        [string]$AreaPath
+        [string]$Story = $null
     )
-    if ($AreaPath) {
-        $script:Config.AreaPath = $AreaPath
-        SaveConfig
-        return
-    }
     $command = "git commit $(if($All){'-a '})-m `"$($Type -join '') $($(if($Story){"#$Story "}))$Message`""
     Write-Host "> " -ForegroundColor DarkBlue -NoNewline
     Write-Host $command -ForegroundColor Yellow
@@ -70,13 +31,6 @@ $gitmojiTypeCompleter = {
     @($codeMatches) + $codeContains + $descContains + $abbreviations | Sort-Object -Unique
 }
 Register-ArgumentCompleter -CommandName gitmoji -ParameterName Type -ScriptBlock $gitmojiTypeCompleter
-
-$storyIdCompleter = {
-    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
-    [scriptblock]$RenderHint = { "'$($_.id)' <# $($_.title) #> " }
-    return GetActiveStories | ForEach-Object $RenderHint
-}
-Register-ArgumentCompleter -CommandName gitmoji -ParameterName Story -ScriptBlock $storyIdCompleter
 
 
 Export-ModuleMember -Function @(
